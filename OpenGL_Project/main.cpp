04254a5fd1a2aa67ead01ced7be26c522b49fcec
Salvador <salvador.karakachoff@gmail.com>
2023-05-28 00:13:40 -0300

    EBO Implementation
    I finally decided to use EBO because it is a basic tool and i had to give it a chance

--- OpenGL_Project/main.cpp
+++ OpenGL_Project/main.cpp
@@ -9,8 +9,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height); //res
 
 void processInput(GLFWwindow* window); //input control
 
-
-//settings
+// settings
 const unsigned int width = 800;
 const unsigned int height = 600;
 
@@ -23,9 +22,9 @@ const char* fragmentShaderOrangeSource = fragmentShaderOrangeCode.c_str();
 std::string fragmentShaderRedCode = readShaderFromFile("fragmentShaderRed.frag");
 const char* fragmentShaderRedSource = fragmentShaderRedCode.c_str();
 
-int main() {
-
-    //GLFW Initialization
+int main()
+{
+    // GLFW Initialization
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -34,10 +33,9 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    //Window creation
-
-    GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL_Basic_Engine", NULL, NULL);
-    if (window == NULL)
+    // Window creation
+    GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL_Basic_Engine", nullptr, nullptr);
+    if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -45,151 +43,142 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
-    //GLAD Initialization
+    // GLAD Initialization
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    //Defining viewport
+    // Defining viewport
     glViewport(0, 0, width, height);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    // Build and compile our shader program
+    // Vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
-    //check for shader compile errors
-    int  success;
+    // Check for shader compile errors
+    int success;
     char infoLog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (!success)
     {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-    //fragment shaders 1
+    // Fragment shaders 1
     unsigned int fragmentShaderOrange = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShaderOrange, 1, &fragmentShaderOrangeSource, NULL);
+    glShaderSource(fragmentShaderOrange, 1, &fragmentShaderOrangeSource, nullptr);
     glCompileShader(fragmentShaderOrange);
-    // check for shader compile errors
+    // Check for shader compile errors
     glGetShaderiv(fragmentShaderOrange, GL_COMPILE_STATUS, &success);
     if (!success)
     {
-        glGetShaderInfoLog(fragmentShaderOrange, 512, NULL, infoLog);
+        glGetShaderInfoLog(fragmentShaderOrange, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-    //fragment shaders 2
+    // Fragment shaders 2
     unsigned int fragmentShaderRed = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShaderRed, 1, &fragmentShaderRedSource, NULL);
+    glShaderSource(fragmentShaderRed, 1, &fragmentShaderRedSource, nullptr);
     glCompileShader(fragmentShaderRed);
-    // check for shader compile errors
+    // Check for shader compile errors
     glGetShaderiv(fragmentShaderRed, GL_COMPILE_STATUS, &success);
     if (!success)
     {
-        glGetShaderInfoLog(fragmentShaderRed, 512, NULL, infoLog);
+        glGetShaderInfoLog(fragmentShaderRed, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-    //shader programs
-     unsigned int shaderProgramOrange = glCreateProgram(); //creates a program and returns it's ID
-     unsigned int shaderProgramRed = glCreateProgram();
+    // Shader programs
+    unsigned int shaderProgramOrange = glCreateProgram(); // Creates a program and returns its ID
+    unsigned int shaderProgramRed = glCreateProgram();
 
     glAttachShader(shaderProgramOrange, vertexShader);
     glAttachShader(shaderProgramOrange, fragmentShaderOrange);
     glLinkProgram(shaderProgramOrange);
-    // check for linking errors
+    // Check for linking errors
     glGetProgramiv(shaderProgramOrange, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgramOrange, 512, NULL, infoLog);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgramOrange, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
-
 
     glAttachShader(shaderProgramRed, vertexShader);
     glAttachShader(shaderProgramRed, fragmentShaderRed);
     glLinkProgram(shaderProgramRed);
-    // check for linking errors
+    // Check for linking errors
     glGetProgramiv(shaderProgramRed, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgramRed, 512, NULL, infoLog);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgramRed, 512, nullptr, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
 
-
-    //delete the shader objects
+    // Delete the shader objects
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShaderOrange);
     glDeleteShader(fragmentShaderRed);
 
-    // set up vertex data. configure vertex attributes
-    float triangle_1[] = {
-        -0.9f, -0.5f, 0.0f,  // left 
-        -0.0f, -0.5f, 0.0f,  // right
-        -0.45f, 0.5f, 0.0f,  // top 
+    // Set up vertex data. Configure vertex attributes
+    float vertices[] = {
+        -0.9f, -0.5f, 0.0f,  // bottom left 
+        -0.45f, 0.5f, 0.0f,  // top left 
+        0.0f, -0.5f, 0.0f,  // bottom mid
+        0.9f, -0.5f, 0.0f,  // bottom right
+        0.45f, 0.5f, 0.0f   // top right
     };
 
-    float triangle_2[] = {
-        0.0f, -0.5f, 0.0f,  // left
-        0.9f, -0.5f, 0.0f,  // right
-        0.45f, 0.5f, 0.0f   // top 
+    unsigned int indices[] = {
+        0, 1, 2, // Triangle 1
+        2, 3, 4  // Triangle 2
     };
 
+    unsigned int VBO, VAO, EBO; // Buffers
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &VAO);
 
-    unsigned int VBOs[2], VAOs[2]; //Buffer(s)
-    glGenBuffers(2, VBOs);
-    glGenVertexArrays(2, VAOs);
-
-    // ..:: Initialization code for triangle_1 (done once (unless your object frequently changes)) :: ..
-    // 1. bind Vertex Array Object
-    glBindVertexArray(VAOs[0]);
-    // 2. copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_1), triangle_1, GL_STATIC_DRAW);
-    // 3. then set our vertex attributes pointers
+    // Initialization code for triangle_1 (done once unless your object frequently changes)
+    // 1. Bind Vertex Array Object
+    glBindVertexArray(VAO);
+    // 2. Copy our vertices array into a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. Load the Element array buffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 4. Set our vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
 
-    // ..:: Initialization code for triangle_2 (done once (unless your object frequently changes)) :: ..
-    // 1. bind Vertex Array Object
-    glBindVertexArray(VAOs[1]);
-    // 2. copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_2), triangle_2, GL_STATIC_DRAW);
-    // 3. then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glBindVertexArray(0);
-
-    //renderloop
+    // Render loop
     while (!glfwWindowShouldClose(window))
     {
+        // Input
         processInput(window);
 
-        //rendering
+        // Rendering commands
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //5. TRIANGLES! :D
 
-        //4. TRIANGLES!
-        //triagle_1 using red shaders
-        glUseProgram(shaderProgramRed);
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        //trianlge_2 using orange shaders
+        // Draw the first triangle using the orange shader program
         glUseProgram(shaderProgramOrange);
-        glBindVertexArray(VAOs[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
+        // Draw the second triangle using the red shader program
+        glUseProgram(shaderProgramRed);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * sizeof(unsigned int)));
+
+        // Check and call events and swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -198,16 +187,13 @@ int main() {
     return 0;
 }
 
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) //resize viewport
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-
-void processInput(GLFWwindow* window) //input control
+void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true); //close the window when escape ir pressed
+        glfwSetWindowShouldClose(window, true);
 }
-
